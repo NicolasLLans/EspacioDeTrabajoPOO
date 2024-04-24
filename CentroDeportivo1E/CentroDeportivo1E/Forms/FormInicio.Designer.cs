9e0e84227dbefcc59656e03df7ee66731f3aaf03
Oscar Sosa <okijulian0791@gmail.com>
2024-04-24 20:59:11 -0300

    se crea proyect con formulario y modelo personas y empleados

---
+++ CentroDeportivo1E/CentroDeportivo1E/Forms/FormInicio.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace CentroDeportivo1E.Forms
+{
+    partial class FormInicio
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            txtBienvenido = new TextBox();
+            SuspendLayout();
+            // 
+            // txtBienvenido
+            // 
+            txtBienvenido.CausesValidation = false;
+            txtBienvenido.Enabled = false;
+            txtBienvenido.Font = new Font("Sans Serif Collection", 15.75F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
+            txtBienvenido.Location = new Point(102, 12);
+            txtBienvenido.Name = "txtBienvenido";
+            txtBienvenido.ReadOnly = true;
+            txtBienvenido.Size = new Size(858, 59);
+            txtBienvenido.TabIndex = 0;
+            txtBienvenido.TextAlign = HorizontalAlignment.Center;
+            // 
+            // FormInicio
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1074, 613);
+            Controls.Add(txtBienvenido);
+            Name = "FormInicio";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "INICIO";
+            Load += FormInicio_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox txtBienvenido;
+    }
+}
---
+++ CentroDeportivo1E/CentroDeportivo1E/Models/Empleado.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CentroDeportivo1E.Models
+{
+    internal class Empleado:Persona
+    {
+        public new int Id { get; set; } 
+        public string Usuario { get; set; }
+        public string Contrasena { get; set; }
+        public DateTime FechaAlta { get; set; }
+        public DateTime FechaBaja { get; set; }
+        public bool baja { get; set; } 
+
+        // Constructor Vacio
+        public Empleado()
+        {
+           
+        }
+
+        public override string ToString()
+        {
+            return $"ID: {Id}, Nombre: {Nombre}, Apellido: {Apellido}, Teléfono: {Telefono}, Número de Socio: {NumeroSocio}, Estado de Pago: {(EstadoPago ? "Pagado" : "Pendiente")}, Usuario: {Usuario}, Fecha de Alta: {FechaAlta}, Fecha de Baja: {FechaBaja}, Baja: {(baja ? "Sí" : "No")}"; // Cambiado a minúscula
+        }
+    }
+}
