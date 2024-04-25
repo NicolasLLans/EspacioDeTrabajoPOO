3b20887217d2540d60db304d904434624da64b1e
Oscar Sosa <okijulian0791@gmail.com>
2024-04-24 21:16:31 -0300

    se vuelven a subir archivos

---
+++ CentroDeportivo1E/Models/Empleado.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CentroDeportivo1E.Models
+{
+    internal class Empleado:Persona
+    {
+        public new int Id { get; set; } // Oculta la propiedad Id de la clase base Persona
+        public string Usuario { get; set; }
+        public string Contrasena { get; set; }
+        public DateTime FechaAlta { get; set; }
+        public DateTime FechaBaja { get; set; }
+        public bool baja { get; set; } // Cambiado a minúscula
+
+        // Constructor
+        public Empleado()
+        {
+            // Inicializar propiedades si es necesario
+        }
+
+        // Método ToString para representar la información del empleado como una cadena
+        public override string ToString()
+        {
+            return $"ID: {Id}, Nombre: {Nombre}, Apellido: {Apellido}, Teléfono: {Telefono}, Número de Socio: {NumeroSocio}, Estado de Pago: {(EstadoPago ? "Pagado" : "Pendiente")}, Usuario: {Usuario}, Fecha de Alta: {FechaAlta}, Fecha de Baja: {FechaBaja}, Baja: {(baja ? "Sí" : "No")}"; // Cambiado a minúscula
+        }
+    }
+}
---
+++ CentroDeportivo1E/Program.cs
@@ -0,0 +1,34 @@
+using CentroDeportivo1E.Forms;
+
+namespace CentroDeportivo1E
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+           
+            
+            bool modDebug= false;
+
+            if (modDebug ==false) { 
+            FormLogin login = new FormLogin();
+
+            if (login.ShowDialog() == DialogResult.OK)
+            {
+                Application.Run(new FormInicio());
+                }
+                else
+                {
+                    Application.Exit();
+                }
+            }
+        }
+    }
+}
