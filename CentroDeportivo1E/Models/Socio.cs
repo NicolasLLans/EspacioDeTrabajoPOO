f6c93632adf609d94295691ae7f187b2fe7c2831
Luciano Madroñal <lucianoumc1@gmail.com>
2024-04-28 19:11:40 -0300

    Cree la clase Socio, segun los atributos del diagrama, agregue su herencia y su metodo constructor y relice la sobrecarga de su metodo ToString - Faltan agregar sus Metodos

---
+++ CentroDeportivo1E/Models/Socio.cs
@@ -0,0 +1,23 @@
+﻿using CentroDeportivo1E.Models;
+
+namespace CentroDeportivo1E.Models
+{
+    internal class Socio : Persona
+    {
+        public int NumeroSocio { get; set; }
+        public float CuotaMensual { get; set; }
+        public bool Activo { get; set; }
+
+        public Socio(string nombre, string apellido, string direccion, long telefono, string email, string tipo, int numeroSocio, float cuotaMensual, bool activo) : base(nombre, apellido, direccion, telefono, email, tipo)
+        {
+            NumeroSocio = numeroSocio;
+            CuotaMensual = cuotaMensual;
+            Activo = activo;
+        }
+
+        public override string ToString()
+        {
+            return $"ID: {Id}, Nombre: {Nombre}, Apellido: {Apellido}, Teléfono: {Telefono}, NumeroSocio: {NumeroSocio}, Cuota mensual: {CuotaMensual}, Estado: {(Activo ? "Activo": "Impago")} ";
+        }
+    }
+}
