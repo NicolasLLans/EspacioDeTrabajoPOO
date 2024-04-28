923662319e68aff1f1204777f878ac71c362fb0a
Luciano Madroñal <lucianoumc1@gmail.com>
2024-04-28 19:39:13 -0300

    Modificacion de la clase Empleado, se actualizaron los get y set y se arreglaron los atributos que hereda en su constructor - ES NECESARIO CORREGIR LOS FORMS QUE USAN LAS CLASES MODIFICADAS

--- CentroDeportivo1E/Models/Empleado.cs
+++ CentroDeportivo1E/Models/Empleado.cs
@@ -1,71 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CentroDeportivo1E.Models
+﻿namespace CentroDeportivo1E.Models
 {
     internal class Empleado : Persona
 
-
     {
-        private int id;
-        private string usuario;
-        private string contrasena;
-        private string puesto;
-        private DateTime fechaAlta;
-        private DateTime fechaBaja;
-        private bool baja;
+        public int Id { get; set; }
+        public string Usuario { get; set; }
+        public string Contrasena { get; set; }
+        public string Puesto { get; set; }
+        public DateTime FechaAlta { get; set; }
+        public DateTime FechaBaja { get; set; }
+        public bool Baja { get; set; }
 
         private static int ultimoId = 0;
-
-
-        public int Id
-        {
-            get { return id; }
-            private set { id = value; }
-        }
-        public string Usuario
-        {
-            get { return usuario; }
-            set { usuario = value; }
-        }
-        public string Contrasena
-        {
-            get { return contrasena; }
-            set { contrasena = value; }
-        }
-        public string Puesto
-        {
-            get { return puesto; }
-            set { puesto = value; }
-        }
-        public DateTime FechaAlta
-        {
-            get { return fechaAlta; }
-            set { fechaAlta = value; }
-        }
-        public DateTime FechaBaja
-        {
-            get { return fechaBaja; }
-            set { fechaBaja = value; }
-        }
-        public bool Baja
-        {
-            get { return baja; }
-            set { baja = value; }
-        }
 
         // Constructor Vacio
         public Empleado()
         {
             Id = ++ultimoId;
         }
-
-        public Empleado(string nombre, string apellido, long telefono, int numeroSocio, bool estadoPago,
+        public Empleado(string nombre, string apellido, string direccion, long telefono, string email, string tipo,
                 string usuario, string contrasena, string puesto, DateTime fechaAlta, DateTime fechaBaja, bool baja)
-                : base(nombre, apellido, telefono, numeroSocio, estadoPago)
+                : base(nombre, apellido, direccion, telefono, email, tipo)
         {
             Usuario = usuario;
             Contrasena = contrasena;
@@ -77,10 +32,8 @@ namespace CentroDeportivo1E.Models
 
         public override string ToString()
         {
-            return $"ID: {Id}, Nombre: {Nombre}, Apellido: {Apellido}, Teléfono: {Telefono}, " +
-                   $"Número de Socio: {NumeroSocio}, Estado de Pago: {(EstadoPago ? "Pagado" : "Pendiente")}, " +
-                   $"Usuario: {Usuario}, Puesto: {Puesto}, Fecha de Alta: {FechaAlta}, Fecha de Baja: {FechaBaja}, " +
-                   $"Baja: {(Baja ? "Sí" : "No")}";
+
+            return $"ID: {Id}, Nombre: {Nombre}, Apellido: {Apellido}, Teléfono: {Telefono}, Direccion: {Direccion}, Email: {Email}, Usuario: {Usuario}, Puesto: {Puesto}, Fecha de Alta: {FechaAlta}, Fecha de Baja: {FechaBaja}" + $"Baja: {(Baja ? "Sí" : "No")}";
         }
 
 
