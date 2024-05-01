b671b46102ab6d3c02e0350e7cdc69cff5f8b0b7
NicolasLLans <nicolaslautaro22@gmail.com>
2024-04-30 21:45:17 -0300

    feat:creación de la función inscribirActividad

--- CentroDeportivo1E/Models/ClubDeportivo.cs
+++ CentroDeportivo1E/Models/ClubDeportivo.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CentroDeportivo1E.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,22 @@ namespace CentroDeportivo1E.Models
 {
     internal class ClubDeportivo
     {
+        private List<Socio>? listaDeSocios;
+
+        public void inscribirActividad(Socio socio, Actividad nuevaActividad)
+        {
+            int CantActividades = socio.Actividades.Count;
+            Console.WriteLine($"El socio {socio.Nombre} tiene {CantActividades} actividades inscritas.");
+
+            if (socio.Actividades.Count < 3)
+            {
+                socio.Actividades.Add(nuevaActividad);
+                Console.WriteLine($"Se ha inscrito la actividad '{nuevaActividad.Nombre}' al socio {socio.Nombre}.");
+            }
+            else
+            {
+                Console.WriteLine($"El socio {socio.Nombre} ya tiene el máximo de actividades inscritas.");
+            }
+        }
     }
 }
